a2ce8ae14fc26ed646e1286bd2aa27f85909af4a
ShawnG <shawngway@gmail.com>
2019-08-18 22:25:40 -0400

    various functions, still very inefficient

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,12 @@ var gameHidden = 0;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
+
+var A1;
+var A2;
+var A3;
+var A4;
+
 function initialize() {
     gameHidden = 0;
     correct = 0;
@@ -16,7 +22,8 @@ var Questions = {
         b: "Gears 2",
         c: "Gears 3",
         d: "Gears 4",
-        
+        //a
+
     },
     Question2: {
         read: "how to beat Psycho Mantis MGs1",
@@ -24,6 +31,7 @@ var Questions = {
         b: "grenade launcher",
         c: "cqc",
         d: "connect remote to port 2 so he doesn't controll then have fun",
+        //d
     },
     Question3: {
         read: "Blue guy with cannon for an arm",
@@ -31,6 +39,7 @@ var Questions = {
         b: "Metroid",
         c: "mario",
         d: "Mega Man",
+        //d
     },
     Question4: {
         read: "First true Br style game that began the flood of the genre",
@@ -38,6 +47,7 @@ var Questions = {
         b: "fortnite",
         c: "H1Z1",
         d: "Arma III",
+        //c
     },
     Questions5: {
         read: "burst rifle name in Halo 2",
@@ -45,6 +55,7 @@ var Questions = {
         b: "needler",
         c: "hand Cannon",
         d: "M4",
+        //a
     },
     Question6: {
         read: "Mario is an ______ plumber",
@@ -52,6 +63,7 @@ var Questions = {
         b: "jewish",
         c: "italian",
         d: "spanish",
+        //c
     },
     Questions7: {
         read: "In a MOBA the center lane player is called",
@@ -59,6 +71,7 @@ var Questions = {
         b: "attack",
         c: "carry",
         d: "farmer",
+        //a
     },
     Questions8: {
         read: "the bolt action sniper in Counter Strike Global defense",
@@ -66,6 +79,7 @@ var Questions = {
         b: "M249",
         c: "Intervention",
         d: "M1",
+        //a
     }
 
 }
@@ -73,10 +87,10 @@ var Questions = {
 function renderQuestion1() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    A1 = $("<button class='Q1 a1'>");
+    A2 = $("<button class='Q1 b'>");
+    A3 = $("<button class='Q1 c'>");
+    A4 = $("<button class='Q1 d'>");
     Qs.text(Questions.Question1.read);
     A1.text(Questions.Question1.a);
     A2.text(Questions.Question1.b);
@@ -92,10 +106,10 @@ function renderQuestion1() {
 function renderQuestion2() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q2 a2'>");
+    var A2 = $("<button class='Q2 b2'>");
+    var A3 = $("<button class='Q2 c2'>");
+    var A4 = $("<button class='Q2 d2'>");
     Qs.text(Questions.Question2.read);
     A1.text(Questions.Question2.a);
     A2.text(Questions.Question2.b);
@@ -111,10 +125,10 @@ function renderQuestion2() {
 function renderQuestion3() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q3 a'>");
+    var A2 = $("<button class='Q3 b'>");
+    var A3 = $("<button class='Q3 c'>");
+    var A4 = $("<button class='Q3 d'>");
     Qs.text(Questions.Question3.read);
     A1.text(Questions.Question3.a);
     A2.text(Questions.Question3.b);
@@ -130,10 +144,10 @@ function renderQuestion3() {
 function renderQuestion4() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>")4
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q4 a'>");
+    var A2 = $("<button class='Q4 b'>");
+    var A3 = $("<button class='Q4 c'>");
+    var A4 = $("<button class='Q4 d'>");
     Qs.text(Questions.Question4.read);
     A1.text(Questions.Question4.a);
     A2.text(Questions.Question4.b);
@@ -149,10 +163,10 @@ function renderQuestion4() {
 function renderQuestion5() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q5 a'>");
+    var A2 = $("<button class='Q5 b'>");
+    var A3 = $("<button class='Q5 c'>");
+    var A4 = $("<button class='Q5 d'>");
     Qs.text(Questions.Question5.read);
     A1.text(Questions.Question5.a);
     A2.text(Questions.Question5.b);
@@ -168,10 +182,10 @@ function renderQuestion5() {
 function renderQuestion6() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q6 a'>");
+    var A2 = $("<button class='Q6 b'>");
+    var A3 = $("<button class='Q6 c'>");
+    var A4 = $("<button class='Q6 d'>");
     Qs.text(Questions.Question6.read);
     A1.text(Questions.Question6.a);
     A2.text(Questions.Question6.b);
@@ -187,10 +201,10 @@ function renderQuestion6() {
 function renderQuestion7() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q7 a'>");
+    var A2 = $("<button class='Q7 b'>");
+    var A3 = $("<button class='Q7 c'>");
+    var A4 = $("<button class='Q7 d'>");
     Qs.text(Questions.Question7.read);
     A1.text(Questions.Question7.a);
     A2.text(Questions.Question7.b);
@@ -206,10 +220,10 @@ function renderQuestion7() {
 function renderQuestion8() {
     $(".question").empty();
     var Qs = $("<h3>");
-    var A1 = $("<button>");
-    var A2 = $("<button>");
-    var A3 = $("<button>");
-    var A4 = $("<button>");
+    var A1 = $("<button class='Q8 a'>");
+    var A2 = $("<button class='Q8 b'>");
+    var A3 = $("<button class='Q8 c'>");
+    var A4 = $("<button class='Q8 d'>");
     Qs.text(Questions.Question8.read);
     A1.text(Questions.Question8.a);
     A2.text(Questions.Question8.b);
@@ -222,4 +236,72 @@ function renderQuestion8() {
     $(".question").append(A4);
 
 }
+console.log(correct);
+renderQuestion1();
+$(".a1").on("click", function () {
+    if (gameHidden === 0) {
+        gameHidden++;
+        correct++;
+        console.log("correct: " + correct);
+        console.log("gameHidden: " + gameHidden);
+        renderQuestion2();
+    }
+
+});
+$(".b1").on("click", function (){
+    if (gameHidden === 0) {
+        gameHidden++;
+        incorrect++;
+        console.log("incorrect: " + incorrect);
+        renderQuestion2();
+    }
+    else if (gameHidden === 1) {
+        gameHidden++;
+        incorrect++;
+        console.log("incorrect: " + icorrect);
+        renderQuestion3();
+    }
+
+});
+$(".c1").on("click", function (){
+    if (gameHidden === 0) {
+        gameHidden++;
+        incorrect++;
+        console.log("incorrect: " + incorrect);
+        renderQuestion2();
+    }
+    else if (gameHidden === 1) {
+        gameHidden++;
+        incorrect++;
+        console.log("incorrect: " + icorrect);
+        renderQuestion3();
+    }
+
+});
+$(".d1").on("click", function (){
+    if (gameHidden === 0) {
+        gameHidden++;
+        incorrect++;
+        console.log("incorrect: " + incorrect);
+        renderQuestion2();
+    }
+    else if (gameHidden === 1) {
+        gameHidden++;
+        correct++;
+        console.log("correct: " + correct);
+        renderQuestion3();
+    }
+
+});
+$(".a2").on("click", function () {
+    if (gameHidden === 1) {
+        gameHidden++;
+        correct++;
+        console.log("correct: " + correct);
+        renderQuestion3();
+    }
+
+});
+
+
 
